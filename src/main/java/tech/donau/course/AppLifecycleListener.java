f3a4dab0753a9906f2fa100f821348099d3b22c1
MRasool <rasoolavazeh@yahoo.com>
2020-11-30 12:20:54 +0330

    Initialization Start and Stop

---
+++ src/main/java/tech/donau/course/AppLifecycleListener.java
@@ -0,0 +1,23 @@
+package tech.donau.course;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class AppLifecycleListener {
+
+    public static Logger LOGGER = Logger.getLogger("Lifecycle");
+
+    public void onStart(@Observes StartupEvent startupEvent) {
+        LOGGER.info("onStart executed");
+        
+    }
+
+    public void onStop(@Observes ShutdownEvent shutdownEvent) {
+        LOGGER.info("onStop executed");
+    }
+}
